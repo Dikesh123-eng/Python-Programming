23ca15c6913d1a70574c3a51a1e5105e459d5a66
Dikesh Singh <Dikeshsingh512@gmail.com>
2025-03-06 05:52:29 -0800

    Add files via upload

---
+++ Dpp_solution_dictonary.py
@@ -0,0 +1,14 @@
+# introduction of dictionarry
+"""
+student={"Name":"Dikesh","Age":23,"Gender":"Male","REG_NO":23153132035,"BRANCH":"CSE-DS"}
+print(student)
+print(type(student))
+"""
+#iteration in Dictonary
+#for keys
+student={"Name":"Dikesh","Age":23,"Gender":"Male","REG_NO":23153132035,"BRANCH":"CSE-DS"}
+for i in student:
+    print(i)
+    #for values
+    for i in student:
+      print(student[i])
