4d453c24f6ec2e50051b93d403e29dede831c039
Dikesh Singh <Dikeshsingh512@gmail.com>
2025-03-06 05:51:58 -0800

    Add files via upload

---
+++ dpp_solution_sets.py
@@ -0,0 +1,22 @@
+#Q1.Write A program to find maxm And minm in a sets
+"""
+a = {3, 6, 7, 8, 3, 12} 
+maxm=max(a)
+minm=min(a)
+print("the maxium vales is:",maxm)
+print("the Minium vales is:",minm)
+"""
+#Q2.Write A program to find common elments in the Lists Using Sets
+"""
+a=[2,8,7,8,9,2,3]
+b=[2,3,7,8,9,2,3]
+c=[2,67,7,9,2,3]
+print(set(a)&set(b)&set(c))
+"""
+#Q3. Write a program to find Differnece betwwen two sets
+a={2,8,7,8,9,2,3}
+b={20,36,7,45,9,2,3}
+x=a.difference(b)
+print(x)
+
+
