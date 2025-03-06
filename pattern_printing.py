232547376545ba7a057d6283afeb67d13fa592dc
Dikesh Singh <Dikeshsingh512@gmail.com>
2025-03-05 21:14:55 -0800

    Add files via upload

---
+++ pattern_printing.py
@@ -0,0 +1,30 @@
+#pattern printing
+"""""
+for i in range (1,5):
+    for j in range (1,5):
+        print("* ",end=" ")
+    print()
+    """
+#pattern printing 2
+""""
+for i in range (1,5):
+    for j in range (1,i+1):
+        print("* ",end=" ")
+    print()
+    """
+#pattern printing 3
+""""
+for i in range (1,5):
+    for j in range (1,i+1):
+        print(j,end=" ")
+    print()
+    """
+#star printing 4
+for i in range (1,6):
+    for j in range (1,i+1):
+        print(i,end=" ")
+    print()
+
+    
+
+
