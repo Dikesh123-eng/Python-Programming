8d9175a0d458aeb5835df8d9c506e143314136b8
Dikesh Singh <Dikeshsingh512@gmail.com>
2025-03-05 21:13:30 -0800

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,64 @@
+#operatos in python
+
+#Arithmetic Operator
+"""
+print(3+6)#addtion
+print(3-6)#substraction
+print(3*6)#multiplication
+print(3/6)#division
+print(3%6)#modulus
+print(2**8)#exponetion
+print(3//6)#float division
+"""
+#comparission operator
+"""
+print(3==8)
+print(3>=8)
+print(3<8)
+print(3!=8)
+"""
+#logical operators
+"""
+print(5>7 or 5<6)
+print(4<8 and 9>2)
+print(not (4>7 and 4>5))
+
+"""
+#Assignment operator
+"""
+a=8
+a+=7
+print(a)
+a-=8
+print(a)
+a*=8
+print(a)
+a/=8
+print(a)
+"""
+#Identity Operator
+"""
+a=567
+b="dikesh"
+print(a is b)
+c=5.7
+print(b is not c)
+"""
+#Bitwise operator
+"""
+print(bin(10))
+print(bin(8))
+print(10&8)#bitwsie and
+print(10|8)#bitwise or
+print(10^8)#bitwise xor
+print(10<<2)#left shift
+print(10>>2)#right shift
+"""
+#membership Operator
+a="dikesh"
+print("d" in a)#in type
+print("k" not in a)
+
+
+
+
