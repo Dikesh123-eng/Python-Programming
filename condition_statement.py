733faeb0c84c7ec2f64b4824939dae9dfd76e3c0
Dikesh Singh <Dikeshsingh512@gmail.com>
2025-03-06 05:46:57 -0800

    Add files via upload

---
+++ condition_statement.py
@@ -0,0 +1,23 @@
+#if statments
+"""
+a=50
+if a>=40:
+print(" Tu to maldar hai be!")
+"""
+#If-ELSE stament
+"""
+a=1
+if a>6:
+    print("thanku")
+else:
+    print("dikesh")
+    """
+#if-elif-else satement
+marks=90
+if marks>=80:
+  print("you got A grade")
+elif marks>=60:
+    print("you got B Grade")
+    
+    else:
+        print("sorry Try again")
