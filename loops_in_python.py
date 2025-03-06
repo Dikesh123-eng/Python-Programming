380c48991d872df018869ae1da6fd9095e16e9b9
Dikesh Singh <Dikeshsingh512@gmail.com>
2025-03-05 21:14:15 -0800

    Add files via upload

---
+++ loops_in_python.py
@@ -0,0 +1,15 @@
+# Loops in python
+# for loop
+"""
+for i in range (1,6,2):
+    print(i)
+    for i in range (1,50):
+        print("Dikesh")
+        #Table in python
+        """
+#while loop
+a=0
+while a<=10:
+    print(a)
+    a+=1
+    #while true loop
